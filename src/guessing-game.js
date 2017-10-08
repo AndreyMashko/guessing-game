8a398023b5d38287e065e37d0788dbab16d22190
AndreyMashko <andrey.mashko.bsu@gmail.com>
2017-10-08 17:26:36 +0400

    all tests passed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		let min;
+		let max;
+		let number;
+	}
 
     setRange(min, max) {
-
+		this.min = min;
+		this.max = max;
     }
 
     guess() {
-
+		this.number = Math.ceil((this.min + this.max) / 2);
+		return this.number;
     }
 
     lower() {
-
+		this.setRange(this.min, this.number);
     }
 
     greater() {
-
+		this.setRange(this.number, this.max);
     }
 }
 
